95b69f79849438e8ab24df8dff6992f1d1162210
romeroraven <romeroravenr@gmail.com>
2021-02-13 18:50:45 -0700

    commit tree b516df685db53ca52afee86e27e4357ea453fe05

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -12,11 +12,11 @@ class PostsController < ApplicationController
 	end
 
 	def create
-	  @post = Post.new
-	  @post.title = params[:title]
-	  @post.description = params[:description]
-	  @post.save
-	  redirect_to post_path(@post)
+	@post = Post.new
+	@post.title = params[:title]
+	@post.description = params[:description]
+	@post.save
+	redirect_to post_path(@post)
 	end
 
 	def edit
@@ -24,8 +24,8 @@ class PostsController < ApplicationController
 	end
 
 	def update
-	  @post = Post.find(params[:id])
-	  @post.update(title: params[:title], description: params[:description])
-	  redirect_to post_path(@post)
+	@post = Post.find(params[:id])
+	@post.update(params.require(:post).permit(:title, :description)) 
+	redirect_to post_path(@post)
 	end
 end
